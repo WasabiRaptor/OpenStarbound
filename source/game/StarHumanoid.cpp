feed1d1956f51b03a0b2ccf08737f51da2c85b57
WasabiRaptor <ketchupraptor@gmail.com>
2025-10-13 11:26:00 -0400

    Update StarHumanoid.cpp

--- source/game/StarHumanoid.cpp
+++ source/game/StarHumanoid.cpp
@@ -397,7 +397,7 @@ void Humanoid::setIdentity(HumanoidIdentity const& identity) {
         return StringView("default");
       });
 
-      m_networkedAnimator.setLocalTag(p.first, valid ? applied.value() : "");
+      m_networkedAnimator.setLocalTag(p.first, valid ? applied.value(p.second) : "");
     }
   }
 }
