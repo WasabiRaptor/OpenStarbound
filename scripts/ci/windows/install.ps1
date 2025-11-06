a7ca58fc387ae36b3776d6081cfaec26a7f7c61e
WasabiRaptor <ketchupraptor@gmail.com>
2025-11-06 06:02:12 -0500

    improved install script

--- scripts/ci/windows/install.ps1
+++ scripts/ci/windows/install.ps1
@@ -2,8 +2,9 @@ $DID_FIND_STARBOUND = 0
 $STARBOUND_INSTALL_PATH = ""
 
 if ($args.Count -ne 0) {
-    $STARBOUND_INSTALL_PATH = $args -join " " -replace '"',''
+  $STARBOUND_INSTALL_PATH = $args -join " " -replace '"',''
 } else {
+  do {
     # We don't know what drive the user has Steam installed to, so we'll need to figure it out ourselves.
     $DRIVE_LETTERS = 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
     $FOUND_STEAM = 0
@@ -18,11 +19,8 @@ if ($args.Count -ne 0) {
     }
 
     if ($FOUND_STEAM -eq 0) {
-        Write-Host "Couldn't find Steam!"
-        Write-Host ""
-        Write-Host "You may manually specify a Starbound installation path instead by re-running this script like so:"
-        Write-Host "`tinstall.bat C:\full\path\to\starbound\root\dir"
-        exit 1
+      Write-Host "Couldn't find Steam!"
+      break
     }
 
     Write-Host "Automatically found Steam: ${DRIVE_LETTER}:\\Program Files (x86)\\Steam\\"
@@ -39,47 +37,56 @@ if ($args.Count -ne 0) {
 
     # Iterate through STEAM_LIBPATHS and look for a path/appid pair.
     foreach ($CUR_PATH in $($STEAM_LIBPATHS -split "`n")) {
-        if ($CUR_PATH -eq ":211820") {
-            $DID_FIND_STARBOUND = 1
-            $STARBOUND_INSTALL_PATH = $LAST_PATH + "\\steamapps\\common\\Starbound"
-            break
-        }
-
-        $LAST_PATH = $CUR_PATH
+      if ($CUR_PATH -eq ":211820") {
+        $DID_FIND_STARBOUND = 1
+        $STARBOUND_INSTALL_PATH = $LAST_PATH + "\\steamapps\\common\\Starbound"
+        break
+      }
+      $LAST_PATH = $CUR_PATH
     }
 
     # If we didn't find the path, bail out and tell the user how they can workaround this if needed.
     if ($DID_FIND_STARBOUND -eq 0) {
-        Write-Host "Couldn't find your Starbound installation!"
-        Write-Host ""
-        Write-Host "Please ensure that your SteamLibrary is setup correctly!"
-        Write-Host "If this is a new installation of Starbound, you may need to restart Steam first for this script to be able to find it."
-        Write-Host "Alternatively, you may manually specify a Starbound installation path by re-running this script like so:"
-        Write-Host "`tinstall.bat C:\full\path\to\starbound\root\dir"
-        exit 1
+      Write-Host "Couldn't find your Starbound installation!"
+      Write-Host ""
+      Write-Host "Please ensure that your SteamLibrary is setup correctly!"
+      Write-Host "If this is a new installation of Starbound, you may need to restart Steam first for this script to be able to find it."
     }
+  } while (0)
 }
 
 if ($DID_FIND_STARBOUND -eq 1) {
     Write-Host "Automatically found your Starbound installation: $STARBOUND_INSTALL_PATH"
 } else {
-    Write-Host "Using manually-specified Starbound installation: $STARBOUND_INSTALL_PATH"
+  if ($STARBOUND_INSTALL_PATH -eq "") {
+    Write-Host ""
+    Write-Host "You may manually specify a Starbound installation path by pasting it here and pressing Enter:"
+    $STARBOUND_INSTALL_PATH = Read-Host "`t"
+
+    if ($STARBOUND_INSTALL_PATH -eq "") {
+      exit 0
+    }
+  }
+  Write-Host "Using manually-specified Starbound installation: $STARBOUND_INSTALL_PATH"
 }
 
 # Make sure whatever path we're using is a valid Starbound installation.
-if (-not (Test-Path "$STARBOUND_INSTALL_PATH\\win64\\starbound.exe")) {
+do {
+  if (-not (Test-Path "$STARBOUND_INSTALL_PATH\\win64\\starbound.exe")) {
     Write-Host "The Starbound installation path does not appear to be valid (missing game executable)!"
     Write-Host ""
 
-    if ($DID_FIND_STARBOUND -eq 1) {
-        Write-Host "You can manually specify your Starbound installation path instead by re-running this script like so:"
-        Write-Host "`tinstall.bat C:\full\path\to\starbound\root\dir"
-    } else {
-        Write-Host "Please double check that the path you typed points towards the root folder of your Starbound installation."
-    }
+    if ($DID_FIND_STARBOUND -eq 0) {
+        Write-Host "You may manually specify a Starbound installation path by pasting it here and pressing Enter:"
+        $STARBOUND_INSTALL_PATH = Read-Host "`t"
 
-    exit 1
-}
+        if ($STARBOUND_INSTALL_PATH -eq "") {
+          exit 0
+        }
+    } continue
+  }
+  break
+} while (1)
 
 # Everything checks out; let's get installing!
 $STEAM_STARBOUND_DIR = $STARBOUND_INSTALL_PATH
