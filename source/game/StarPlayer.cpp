44d377d433e3b8430d348f49ef70e0cc60638413
WasabiRaptor <ketchupraptor@gmail.com>
2026-06-21 16:52:39 -0400

    duplicate entity callbacks

--- source/game/StarPlayer.cpp
+++ source/game/StarPlayer.cpp
@@ -372,7 +372,6 @@ void Player::init(World* world, EntityId entityId, EntityMode mode) {
       p.second->addActorMovementCallbacks(m_movementController.get());
       p.second->addCallbacks("entity", LuaBindings::makeEntityCallbacks(this));
       p.second->addCallbacks("player", LuaBindings::makePlayerCallbacks(this));
-      p.second->addCallbacks("entity", LuaBindings::makeEntityCallbacks(this));
       p.second->addCallbacks("status", LuaBindings::makeStatusControllerCallbacks(m_statusController.get()));
       p.second->addCallbacks("songbook", LuaBindings::makeSongbookCallbacks(m_songbook.get()));
       p.second->addCallbacks("animator", LuaBindings::makeNetworkedAnimatorCallbacks(humanoid()->networkedAnimator()));
